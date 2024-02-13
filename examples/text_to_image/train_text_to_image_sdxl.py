432f0b72bdc5882f6fd8a8746dbe33d37a987a04
Dhomochevisk <114751551+domochevisk@users.noreply.github.com>
2024-02-13 15:59:30 -0300

    Update train_text_to_image_sdxl.py

--- examples/text_to_image/train_text_to_image_sdxl.py
+++ examples/text_to_image/train_text_to_image_sdxl.py
@@ -586,7 +586,7 @@ def main(args):
 
     logging_dir = Path(args.output_dir, args.logging_dir)
 
-    accelerator_project_config = ProjectConfiguration(project_dir=args.output_dir, logging_dir=logging_dir)
+    accelerator_project_config = ProjectConfiguration(project_dir=args.output_dir, project_dir=logging_dir)
 
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
