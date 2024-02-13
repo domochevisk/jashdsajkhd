4960a88b6a06da471501218703459f7d968ef289
Dhomochevisk <114751551+domochevisk@users.noreply.github.com>
2024-02-13 19:50:20 -0300

    Update train_text_to_image_sdxl.py

--- examples/text_to_image/train_text_to_image_sdxl.py
+++ examples/text_to_image/train_text_to_image_sdxl.py
@@ -115,15 +115,6 @@ def import_model_class_from_model_name_or_path(
     )
     model_class = text_encoder_config.architectures[0]
 
-    if model_class == "CLIPTextModel":
-        from transformers import CLIPTextModel
-
-        return CLIPTextModel
-    elif model_class == "CLIPTextModelWithProjection":
-        from transformers import CLIPTextModelWithProjection
-
-        return CLIPTextModelWithProjection
-    else:
         raise ValueError(f"{model_class} is not supported.")
 
 
